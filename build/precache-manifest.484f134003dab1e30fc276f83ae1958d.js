d8a38b6dab602a5cb473d707a71f401e50e82ad0
bhavani902 <govrayapallybhavani@gmail.com>
2022-06-22 22:13:02 +0530

    commit on master

---
+++ build/precache-manifest.484f134003dab1e30fc276f83ae1958d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a3859087a521d2568cf41b1dbad7495",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3fe306ad3637a2c4ac9",
+    "url": "/static/css/main.19d7d230.chunk.css"
+  },
+  {
+    "revision": "53cf59ffe34eacc6e7a6",
+    "url": "/static/js/2.bbccb4b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bbccb4b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3fe306ad3637a2c4ac9",
+    "url": "/static/js/main.94f0657f.chunk.js"
+  },
+  {
+    "revision": "61c89a8c5b3df91766ad",
+    "url": "/static/js/runtime-main.be155184.js"
+  },
+  {
+    "revision": "d698c6929f6bb90d6702e8a31f338975",
+    "url": "/static/media/loading.d698c692.svg"
+  }
+]);
