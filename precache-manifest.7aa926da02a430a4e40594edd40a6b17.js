1d21e63c0fe9c3ad0acee8578b89687a3f84862f
bhavani902 <govrayapallybhavani@gmail.com>
2022-06-22 22:51:29 +0530

    Updates

---
+++ precache-manifest.7aa926da02a430a4e40594edd40a6b17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e49e5cfbbe7e980025b6f77bc7a034",
+    "url": "/my-predictor/index.html"
+  },
+  {
+    "revision": "ba9f88e7fcca27706d33",
+    "url": "/my-predictor/static/css/main.19d7d230.chunk.css"
+  },
+  {
+    "revision": "53cf59ffe34eacc6e7a6",
+    "url": "/my-predictor/static/js/2.bbccb4b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-predictor/static/js/2.bbccb4b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba9f88e7fcca27706d33",
+    "url": "/my-predictor/static/js/main.1698234b.chunk.js"
+  },
+  {
+    "revision": "003780fd93ba5e1064d7",
+    "url": "/my-predictor/static/js/runtime-main.827d4cc6.js"
+  },
+  {
+    "revision": "d698c6929f6bb90d6702e8a31f338975",
+    "url": "/my-predictor/static/media/loading.d698c692.svg"
+  }
+]);
